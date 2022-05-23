fae9c111e4b98b36b2d8dc1be464d2599ef41b78
jasurxamraliyev <jasjasurbek4@gmail.com>
2022-05-23 16:35:25 +0300

    card7

---
+++ src/components/card4.vue
@@ -0,0 +1,99 @@
+<template>
+  <div>
+    <q-card class="my-card">
+      <div class="text-h5 q-ml-md"> By vendors</div><br>
+      <div class="row q-ml-md content-center">
+        <div>
+          <div class="row items-center">
+            <input type="checkbox" name="" id="">
+            <div class="q-ml-sm">aldi</div>
+          </div>
+          <!-- checkbox 2 -->
+          <div class="row items-center">
+            <input type="checkbox" name="" id="">
+            <div class="q-ml-sm">adidas</div>
+          </div>
+          <!-- checkbox 3 -->
+          <div class="row items-center">
+            <input type="checkbox" name="" id="">
+            <div class="q-ml-sm">Burbe</div>
+          </div>
+          <!-- checkbox 4 -->
+          <div class="row items-center">
+            <input type="checkbox" name="" id="">
+            <div class="q-ml-sm">Chanel</div>
+          </div>
+          <!-- checkbox 5 -->
+          <div class="row items-center">
+            <input type="checkbox" name="" id="">
+            <div class="q-ml-sm">Prade</div>
+          </div>
+          <!-- checkbox 6 -->
+          <div class="row items-center">
+            <input type="checkbox" name="" id="">
+            <div class="q-ml-sm">Kroger</div>
+          </div>
+          <!-- checkbox 7 -->
+          <div class="row items-center">
+            <input type="checkbox" name="" id="">
+            <div class="q-ml-sm">Traders</div>
+          </div>
+          <!-- checkbox 8 -->
+          <div class="row items-center">
+            <input type="checkbox" name="" id="">
+            <div class="q-ml-sm">Publix</div>
+          </div>
+        </div>
+        <div class="q-ml-xl">
+          <div>
+          <div class="row items-center">
+            <input type="checkbox" name="" id="">
+            <div class="q-ml-sm">Hseris</div>
+          </div>
+          <!-- checkbox 2 -->
+          <div class="row items-center">
+            <input type="checkbox" name="" id="">
+            <div class="q-ml-sm">Costco</div>
+          </div>
+          <!-- checkbox 3 -->
+          <div class="row items-center">
+            <input type="checkbox" name="" id="">
+            <div class="q-ml-sm">Targets</div>
+          </div>
+          <!-- checkbox 4 -->
+          <div class="row items-center">
+            <input type="checkbox" name="" id="">
+            <div class="q-ml-sm">Creen Tea</div>
+          </div>
+          <!-- checkbox 5 -->
+          <div class="row items-center">
+            <input type="checkbox" name="" id="">
+            <div class="q-ml-sm">ISnack</div>
+          </div>
+          <!-- checkbox 6 -->
+          <div class="row items-center">
+            <input type="checkbox" name="" id="">
+            <div class="q-ml-sm">Pambox</div>
+          </div>
+        </div>
+       </div>
+      </div>
+    </q-card>
+  </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+  .my-card{
+    width:250px;
+    height:240px;
+    border-radius: 10px;
+    border-left: 1px solid greenyellow;
+
+  }
+</style>
--- src/pages/IndexPage.vue
+++ src/pages/IndexPage.vue
@@ -4,8 +4,9 @@
     <card/><br>
     <EssentialLink/><br>
     <Card2/><br>
-    <Card3/>
-
+    <Card3/><br>
+    <Card4/><br>
+    <Card6/>
 
     <div class="q-mt-xl"></div>
   </div>
@@ -17,8 +18,10 @@ import Card1 from '../components/card1.vue';
 import EssentialLink from 'src/components/EssentialLink.vue';
 import Card2 from '../components/card2.vue';
 import Card3 from '../components/card3.vue';
+import Card4 from 'src/components/card4.vue';
+import Card6 from 'src/components/card6.vue';
 export default {
-  components: { card, Card1, EssentialLink, Card2, Card3 },
+  components: { card, Card1, EssentialLink, Card2, Card3, Card4, Card6 },
   component:{card}
 }
 </script>
